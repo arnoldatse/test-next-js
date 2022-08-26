14917a2309976db0dc69ddb0c149d7f2373a3da4
Arnold Atsé <atse.arnold@gmail.com>
2022-08-26 21:02:33 +0000

    refactor(carDetails): removed car details comments

--- pages/cars/car/[id].tsx
+++ pages/cars/car/[id].tsx
@@ -271,45 +271,4 @@ const CarContent: FunctionComponent = () => {
   );
 };
 
-/* export async function getStaticPaths() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/inventory/car/search`
-  );
-  const cars = await res
-    .json()
-    .then(
-      (value: {
-        result: CarListInterface[];
-        pagination: PaginationInterface;
-      }) => value.result
-    )
-    .catch((error: any) => console.log(error));
-
-  const paths = cars?.map((car) => ({
-    params: { id: car.id },
-  }));
-
-  return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }: { params: { id: string } }) {
-  const resCar = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/inventory/car/${params.id}`
-  );
-  const car = await resCar
-    .json()
-    .then((value: CarDetailsInterface) => value)
-    .catch((error: any) => console.log(error));
-
-  const resMedias = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/inventory/car_media?carId=${params.id}`
-  );
-  const medias = await resMedias
-    .json()
-    .then((value: { carMediaList: CarMediaInterface[] }) => value.carMediaList)
-    .catch((error: any) => console.log(error));
-
-  return { props: { car, medias } };
-} */
-
 export default Car;
